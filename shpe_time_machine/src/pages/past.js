1638edf36c11ffec009bd77c7d3d2c8a3c16c50d
Miguel Garcia <miguelgarcia@Miguels-MacBook-Pro-6.local>
2023-04-21 17:47:23 -0500

    added iframe

--- shpe_time_machine/src/pages/past.js
+++ shpe_time_machine/src/pages/past.js
@@ -6,8 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <iframe src="https://www.shpeuhchapter.com/" width="800" height="600" frameborder="0" allowfullscreen></iframe>
-
+        <p> wadup bitches</p>
     </main>
   )
 }
